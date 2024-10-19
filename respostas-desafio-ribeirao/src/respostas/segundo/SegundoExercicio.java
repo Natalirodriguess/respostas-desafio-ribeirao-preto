9366b85a2ca8f8f5dd187516f3839f3ab29ad052
Natali Rodrigues <natalyrodrigues43210@gmail.com>
2024-10-19 17:21:47 -0300

    Segundo e terceiro exercícios

--- respostas-desafio-ribeirao/src/respostas/segundo/SegundoExercicio.java
+++ respostas-desafio-ribeirao/src/respostas/segundo/SegundoExercicio.java
@@ -1,4 +1,34 @@
 package respostas.segundo;
 
+import java.util.Scanner;
+
 public class SegundoExercicio {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe uma palavra: ");
+        String palavra = scanner.nextLine();
+
+        int contador = 0;
+
+
+        for (int i = 0; i < palavra.length(); i++) {
+            char c = palavra.charAt(i);
+            if (c == 'a' || c == 'A') {
+                contador++;
+            }
+        }
+
+
+        if (contador > 0) {
+            System.out.println("A letra 'a' aparece " + contador + " vez(es).");
+        } else {
+            System.out.println("A letra 'a' não aparece na palavra.");
+        }
+
+        scanner.close();
+    }
+
 }
--- respostas-desafio-ribeirao/src/respostas/terceiro/TerceiroExercicio.java
+++ respostas-desafio-ribeirao/src/respostas/terceiro/TerceiroExercicio.java
@@ -1,4 +1,22 @@
 package respostas.terceiro;
 
 public class TerceiroExercicio {
+    public static void main(String[] args) {
+
+        int indice = 12, soma = 0, k = 1;
+
+        while( k < indice){
+
+            k = k + 1;
+
+            soma = soma + k;
+        }
+
+        System.out.println(soma);
+
+    }
+
+
+
+
 }
